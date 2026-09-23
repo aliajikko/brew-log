45237a95ab269c37643dfc9b7019b351f384c3fd
aliajikko <ali.ajeel.rasheed@gmail.com>
2026-09-23 14:03:58 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "brewlog-v2";
+const CACHE = "brewlog-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => {
